2a7576d6f77297b2c53f53cab79af6b03039b942
Camila-Ailen <gcamilaailen@gmail.com>
2023-11-28 16:32:02 -0300

    productos de prueba (factory)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,16 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        Storage::deleteDirectory('products');
+        Storage::makeDirectory('products');
+
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('12345678'),
+        ]);
 
         $this->call([
             FamilySeeder::class,
-            UserSeeder::class,
+            // UserSeeder::class,
         ]);
+
+        Product::factory(150)->create();
     }
 }
