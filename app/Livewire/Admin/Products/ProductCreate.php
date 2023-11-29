3dc7945f0d23a365b5dc459911af937d86427078
Camila-Ailen <gcamilaailen@gmail.com>
2023-11-28 22:06:49 -0300

    termina de crear (con imagen)

--- app/Livewire/Admin/Products/ProductCreate.php
+++ app/Livewire/Admin/Products/ProductCreate.php
@@ -4,16 +4,19 @@ namespace App\Livewire\Admin\Products;
 
 use App\Models\Category;
 use App\Models\Family;
+use App\Models\Product;
 use App\Models\Subcategory;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ProductCreate extends Component
 {
-
+    use WithFileUploads;
     public $families;
     public $family_id = '';
     public $category_id = '';
+    public $image;
 
     public $product = [
         'sku' => '',
@@ -27,6 +30,21 @@ class ProductCreate extends Component
     public function mount()
     {
         $this->families = Family::all();
+    }
+
+    public function boot()
+    {
+        $this->withValidator(function ($validator) {
+            
+            if ($validator->fails()) {
+                $this->dispatch('swal', [
+                    'icon' => 'error',
+                    'title' => 'Oops...',
+                    'text' => 'Algo salió mal!',
+                ]);
+            }
+
+        });
     }
 
     public function updatedFamilyId($value)
@@ -50,6 +68,43 @@ class ProductCreate extends Component
     public function subcategories()
     {
         return Subcategory::where('category_id', $this->category_id)->get();
+    }
+
+    public function store ()
+    {
+        $this->validate([
+            'image' => 'required|image|max:1024',
+            'product.sku' => 'required|unique:products,sku',
+            'product.name' => 'required|max:255',
+            'product.description' => 'nullable',
+            'product.price' => 'required|numeric|min:0',
+            'product.subcategory_id' => 'required|exists:subcategory,id',   
+        ],[],[
+            'product.sku' => 'del codigo',
+            'product.name' => 'nombre',
+            'product.description' => 'descripción',
+            'product.price' => 'precio',
+            'product.subcategory_id' => 'subcategoría',
+        ]);
+
+        $this->product['image_path'] = $this->image->store('products', 'public');
+
+
+        $product = Product::create($this->product);
+
+        session()->flash('swal', [
+            'icon' => 'success',
+            'title' => 'Perfecto!',
+            'text' => 'El producto se ha creado exitosamente',
+        ]);
+
+        return redirect()->route('admin.products.edit', $product);
+
+        // auth()->user()->products()->create($this->product);
+
+        // $this->reset(['product', 'family_id', 'category_id', 'image']);
+
+        // $this->emit('productStored');
     }
 
 
--- resources/views/livewire/admin/products/product-create.blade.php
+++ resources/views/livewire/admin/products/product-create.blade.php
@@ -1,105 +1,161 @@
-<div class="card">
+<div>
 
-    <div class="mb-4">
-        <x-label class="mb-1">
-            Código
-        </x-label>
+    <form wire:submit="store">
 
-        <x-input wire:model="product.sku" 
-            class="w-full"
-            placeholder="Por favor ingrese el codigo del producto" />
-    </div>
-
-    <div class="mb-4">
-        <x-label>
-            Nombre
-        </x-label>
-
-        <x-input wire:model="product.name" 
-            class="w-full"
-            placeholder="Por favor ingrese el nombre del producto" />
-    </div>
-
-    <div class="mb-4">
-        <x-label>
-            Descripción
-        </x-label>
-
-        <x-textarea 
-            wire:model="product.description" 
-            class="w-full"
-            placeholder="Por favor ingrese la descripción del producto">
-        </x-textarea>
-    </div>
-
-    {{-- Lista desplegable de familia --}}
-    <div class="mb-4">
-        <x-label>
-            Familia
-        </x-label>
-
-        <x-select class="w-full" wire:model.live="family_id">
-
-            <option value="" disabled>
-                Seleccione una familia
-            </option>
-
-            @foreach ($families as $family)
-                
-                <option value="{{ $family->id }}">
-                    {{ $family->name }}
-                </option>
-
-            @endforeach
-        </x-select>
     
-    
-    </div>
+        <figure class="mb-4 relative">
 
-    {{-- Lista desplegable de categoria --}}
-    <div class="mb-4">
-        <x-label>
-            Categoria
-        </x-label>
+            <div class="absolute top-8 right-8">
+                <label class="flex items-center px-4 py-2 rounded-lg bg-white cursor-pointer text-gray-700">
+                    <i class="fas fa-camera mr-2"></i>
+                    Actualizar imagen
 
-        <x-select class="w-full" wire:model.live="category_id">
-
-            <option value="" disabled>
-                Seleccione una categoria
-            </option>
-
-            @foreach ($this->categories as $category)
-                
-                <option value="{{ $category->id }}">
-                    {{ $category->name }}
-                </option>
-
-            @endforeach
-        </x-select>
-    
-    
-    </div>
-
-    {{-- Lista desplegable de subcategoria --}}
-    <div class="mb-4">
-        <x-label>
-            Subcategoria
-        </x-label>
-
-        <x-select class="w-full" wire:model.live="product.subcategory_id">
-
-            <option value="" disabled>
-                Seleccione una subcategoria
-            </option>
-
-            @foreach ($this->subcategories as $subcategory)
-                
-                <option value="{{ $subcategory->id }}">
-                    {{ $subcategory->name }}
-                </option>
-
-            @endforeach
-        </x-select>
+                    <input type="file" 
+                        class="hidden" 
+                        accept="image/*"
+                        wire:model="image">
+                </label>
+            </div>
 
 
+            <img class="aspect-[16/9] object-cover object-center w-full" 
+                src="{{ $image ? $image->temporaryUrl() : asset('img/no-image.png') }}" 
+                alt="">
+        </figure>
+
+        <x-validation-errors class="mb-4" />
+
+
+        <div class="card">
+
+            {{-- Campo de codigo --}}
+            <div class="mb-4">
+                <x-label class="mb-1">
+                    Código
+                </x-label>
+        
+                <x-input wire:model="product.sku" 
+                    class="w-full"
+                    placeholder="Por favor ingrese el codigo del producto" />
+            </div>
+        
+            {{-- Campo de nombre --}}
+            <div class="mb-4">
+                <x-label>
+                    Nombre
+                </x-label>
+        
+                <x-input wire:model="product.name" 
+                    class="w-full"
+                    placeholder="Por favor ingrese el nombre del producto" />
+            </div>
+        
+            {{-- Campo de descripcion --}}
+            <div class="mb-4">
+                <x-label>
+                    Descripción
+                </x-label>
+        
+                <x-textarea 
+                    wire:model="product.description" 
+                    class="w-full"
+                    placeholder="Por favor ingrese la descripción del producto">
+                </x-textarea>
+            </div>
+        
+            {{-- Lista desplegable de familia --}}
+            <div class="mb-4">
+                <x-label>
+                    Familia
+                </x-label>
+        
+                <x-select class="w-full" wire:model.live="family_id">
+        
+                    <option value="" disabled>
+                        Seleccione una familia
+                    </option>
+        
+                    @foreach ($families as $family)
+                        
+                        <option value="{{ $family->id }}">
+                            {{ $family->name }}
+                        </option>
+        
+                    @endforeach
+                </x-select>
+            
+            
+            </div>
+        
+            {{-- Lista desplegable de categoria --}}
+            <div class="mb-4">
+                <x-label>
+                    Categoria
+                </x-label>
+        
+                <x-select class="w-full" wire:model.live="category_id">
+        
+                    <option value="" disabled>
+                        Seleccione una categoria
+                    </option>
+        
+                    @foreach ($this->categories as $category)
+                        
+                        <option value="{{ $category->id }}">
+                            {{ $category->name }}
+                        </option>
+        
+                    @endforeach
+                </x-select>
+            
+            
+            </div>
+        
+            {{-- Lista desplegable de subcategoria --}}
+            <div class="mb-4">
+                <x-label>
+                    Subcategoria
+                </x-label>
+        
+                <x-select class="w-full" wire:model.live="product.subcategory_id">
+        
+                    <option value="" disabled>
+                        Seleccione una subcategoria
+                    </option>
+        
+                    @foreach ($this->subcategories as $subcategory)
+                        
+                        <option value="{{ $subcategory->id }}">
+                            {{ $subcategory->name }}
+                        </option>
+        
+                    @endforeach
+                </x-select>
+            </div>
+
+            {{-- Campo de precio --}}
+            <div class="mb-4">
+                <x-label class="mb-1">
+                    Precio
+                </x-label>
+        
+                <x-input 
+                    type="number"
+                    step="0.01"
+                    wire:model="product.price" 
+                    class="w-full"
+                    placeholder="Por favor ingrese el precio del producto" />
+            </div>
+            
+            {{-- Boton para crear --}}
+            <div class="flex justify-end">
+                <x-button>
+                    Crear producto
+                </x-button>
+            </div>
+        
+        </div>
+
+    </form>
 </div>
+
